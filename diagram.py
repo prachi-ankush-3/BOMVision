980ba9e7e6cc4be086278c50deb9a060e2df1bf9
Prachi Ankush <prachiankush3@gmail.com>
2026-07-29 23:20:54 +0530

    first one is limited for 2 cols now multiple columns are acceptable

--- diagram.py
+++ diagram.py
@@ -6,21 +6,32 @@ os.environ["PATH"] += os.pathsep + r"C:\Program Files (x86)\Graphviz\bin"
 print("PATH =", os.environ["PATH"])
 print("DOT =", shutil.which("dot"))
 
+import pandas as pd
 from graphviz import Digraph
 
-
 def create_diagram(df):
-
     dot = Digraph("Architecture")
     dot.attr(rankdir="TB")
 
-    for index, row in df.iterrows():
-        parent = str(row["Parent"])
-        component = str(row["Component"])
+    if len(df.columns) == 2:
 
-        dot.node(parent)
-        dot.node(component)
-        dot.edge(parent, component)
+        for _, row in df.iterrows():
+            parent = str(row.iloc[0])
+            child = str(row.iloc[1])
+
+            dot.node(parent)
+            dot.node(child)
+            dot.edge(parent, child)
+
+    else:
+
+        for _, row in df.iterrows():
+            values = [str(v) for v in row if pd.notna(v)]
+
+            for i in range(len(values)-1):
+                dot.node(values[i])
+                dot.node(values[i+1])
+                dot.edge(values[i], values[i+1])
 
     dot.render("output/architecture", format="png", cleanup=True)
 
